e3c14693a91f03f81b8511c8e0780cc247e8f6c9
Hemanth <131354764+hemanth-92@users.noreply.github.com>
2025-05-18 14:34:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ import requests
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json)
-st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -49,6 +45,10 @@ time_to_insert  = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json)
+st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
 
